6758b0d6d1660750db4ac6465c13d05980f6e21c
Kelvin Wong <kakoo40@gmail.com>
2021-08-09 09:13:45 +1200

    changed get url

--- client/src/components/ShowBookList.js
+++ client/src/components/ShowBookList.js
@@ -14,7 +14,7 @@ class ShowBookList extends Component {
 
   componentDidMount() {
     axios
-      .get('http://localhost:8082/api/books')
+      .get('https://kelvinw-book-database.herokuapp.com/api/books')
       .then(res => {
         this.setState({
           books: res.data
