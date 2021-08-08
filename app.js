41587b24d8a65239997ce0f444e066dcb1210f9e
Kelvin Wong <kakoo40@gmail.com>
2021-08-08 16:12:32 +1200

    Final deployment preperation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
+const path = require('path')
 
 // routes
 const books = require('./routes/api/books');
@@ -16,10 +17,22 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('Api running!'));
 
 // use Routes
 app.use('/api/books', books);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,'/client/build')))
+
+    app.get('*',(req,res) =>{
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    })
+} else {
+    app.get('/',(req,res) =>{
+        res.send("Api running");
+    });
+}
 
 const port = process.env.PORT || 8082;
 
